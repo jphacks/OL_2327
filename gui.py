5b9c463a332c4a4737c76960c05b2f18e57af50c
みぞい <106292301+Mizoi3@users.noreply.github.com>
2023-10-29 10:25:22 +0900

    convert into day2 version

--- gui.py
+++ gui.py
@@ -1,90 +1,52 @@
-﻿#!/usr/bin/env python3
+﻿import tkinter as tk
+from tkinter import Button
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import datetime
 
-import tkinter as tk
-from tkinter import Toplevel, ttk
-from PIL import Image, ImageTk
-import pandas as pd
-import subprocess
-import threading
+class FingerDrawingApp:
+    def __init__(self, root):
+        self.root = root
+        root.title("FingerDrawing")
+        root.geometry("800x1000")
 
-# グローバル変数でウィンドウとボタンのサイズを制御
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 500
-BUTTON_WIDTH = 30
-BUTTON_HEIGHT = 3
-HISTORY_WINDOW_WIDTH = 800
-HISTORY_WINDOW_HEIGHT = 500
-CSV_FILE_PATH = "./data/history.csv"  # CSVファイルのパス
-IMAGE_PATH = "./data/instruction.png"  # 画像ファイルのパス
+        self.title = tk.Label(root, text="FingerDrawing", font=("Helvetica", 36))
+        self.title.pack(pady=10)
 
-def run_main():
-    threading.Thread(target=lambda: subprocess.run(["python", "main.py"])).start()
+        self.title = tk.Label(root, text="2023/10/28-29, JPHACK2023, All Reserve in CambriaExplosion", font=("Helvetica", 14))
+        self.title.pack(pady=5)
 
-def open_history_window():
-    history_window = Toplevel(root)
-    history_window.title("Shortcut History")
-    history_window.grab_set()  # Make history window modal
-    history_window.geometry(f"{HISTORY_WINDOW_WIDTH}x{HISTORY_WINDOW_HEIGHT}")
+        self.fig, self.ax = plt.subplots(figsize=(4, 4))
+        self.ax.axis('off')  # 軸を非表示にする
+        self.canvas = FigureCanvasTkAgg(self.fig, master=root)
+        self.canvas.get_tk_widget().pack()
 
-    tk.Label(history_window, text="Shortcut History", font=("Arial", 16)).pack(pady=10)
+        self.button_frame = tk.Frame(root)
+        self.button_frame.pack(side=tk.BOTTOM, fill=tk.X, pady=10)
 
-    # Create table for shortcut history
-    table = ttk.Treeview(history_window, columns=("Time", "Action"), show="headings")
-    table.heading("Time", text="Time")
-    table.heading("Action", text="Action")
-    table.pack(fill=tk.BOTH, expand=True)  # Fill the window
+        self.quit_button = Button(self.button_frame, text="Quit", command=self.quit, width=10, height=2)
+        self.quit_button.pack(side=tk.LEFT, padx=10, expand=True)
 
-    # Load data from CSV file and populate the table
-    data = pd.read_csv(CSV_FILE_PATH)
-    for index, row in data.iterrows():
-        table.insert("", "end", values=(row["Time"], row["Action"]))
+        self.delete_button = Button(self.button_frame, text="Delete", command=self.delete, width=10, height=2)
+        self.delete_button.pack(side=tk.LEFT, padx=10, expand=True)
 
-    tk.Button(history_window, text="Close", command=history_window.destroy, width=BUTTON_WIDTH, height=BUTTON_HEIGHT).pack(pady=10)
+        self.save_button = Button(self.button_frame, text="Save", command=self.save, width=10, height=2)
+        self.save_button.pack(side=tk.LEFT, padx=10, expand=True)
 
-def exit_app():
-    root.quit()
+    def quit(self):
+        self.root.destroy()
 
-def on_configure(event, canvas):
-    # Update the scrollregion whenever the size of the inner frame changes.
-    canvas.configure(scrollregion=canvas.bbox('all'))
+    def delete(self):
+        self.ax.clear()
+        self.ax.axis('off')  # 軸を再度非表示にする
+        self.canvas.draw()
 
-def main():
-    global root
+    def save(self):
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"drawfinger_{timestamp}.png"
+        self.fig.savefig(filename)
+
+if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Posture Shortcut")
-    root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-
-    canvas = tk.Canvas(root)
-    scrollbar = tk.Scrollbar(root, command=canvas.yview)
-    frame = tk.Frame(canvas)
-
-    canvas.pack(side="left", fill="both", expand=True)
-    scrollbar.pack(side="right", fill="y")
-    canvas.create_window((WINDOW_WIDTH/2, 0), window=frame, anchor="n")  # Center frame horizontally
-
-    frame.bind("<Configure>", lambda event: on_configure(event, canvas))  # Pass canvas as an argument
-
-    # Use grid instead of pack for better centering
-    tk.Label(frame, text="Posture Shortcut", font=("Arial", 36)).grid(row=0, column=0, pady=10)
-    tk.Label(frame, text="Usage", font=("Arial", 24)).grid(row=1, column=0, pady=10)
-
-    # Load and display the image
-    image = Image.open(IMAGE_PATH)
-    new_width = BUTTON_WIDTH * 10  # Assuming each character in the button is about 10 pixels wide
-    new_height = int((new_width / image.width) * image.height)  # Keep the aspect ratio
-    resized_image = image.resize((new_width, new_height))
-    photo = ImageTk.PhotoImage(resized_image)
-    img_label = tk.Label(frame, image=photo)
-    img_label.photo = photo
-    img_label.grid(row=2, column=0, pady=10)  # Use grid
-
-    tk.Button(frame, text="Start", command=run_main, width=BUTTON_WIDTH, height=BUTTON_HEIGHT).grid(row=3, column=0, pady=5)  # Use grid
-    tk.Button(frame, text="Shortcut History", command=open_history_window, width=BUTTON_WIDTH, height=BUTTON_HEIGHT).grid(row=4, column=0, pady=5)  # Use grid
-    tk.Button(frame, text="Exit", command=exit_app, width=BUTTON_WIDTH, height=BUTTON_HEIGHT).grid(row=5, column=0, pady=5)  # Use grid
-
-    # This line is important to link the scrollbar to the canvas
-    canvas.configure(yscrollcommand=scrollbar.set)
+    app = FingerDrawingApp(root)
     root.mainloop()
-
-if __name__ == '__main__':
-    main()
